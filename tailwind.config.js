d11880edc5918fb765c50ce6e84b6c11d8fdc057
Rustam <rustamjon20031101@gmail.com>
2024-09-18 23:19:24 +0500

    Fix issues and push again

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+ /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      "clersky-green": "#198C36",
+      "clersky-green-dark": "#163A24",
+      "clersky-white": "#FBFAF7",
+      "clersky-brown": "#FAB5A0",
+      "clersky-black": "#163A24",
+      "clarsky-bisque": "#ffe4c4"
+    },
+  },
+  plugins: [],
+};
+
